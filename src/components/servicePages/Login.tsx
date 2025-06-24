675263ecb2590ebca77c2b154460ca95dfb15bae
NMarkovskii <nikitamaxi@inbox.ru>
2025-06-24 22:22:59 +0300

    updated component Login

--- src/components/servicePages/Login.tsx
+++ src/components/servicePages/Login.tsx
@@ -4,20 +4,32 @@ import type {LoginData} from "../../utils/shop-types.ts";
 import {useAppDispatch} from "../../redux/hooks";
 import {loginAction} from "../../redux/slices/authSlice";
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import {login} from "../../firebase/firebaseAuthService";
 
 const Login = () => {
     const dispatch = useAppDispatch();
-    const submitFn = (loginData: LoginData)=> {
-        // console.log(JSON.stringify(loginData))
-        dispatch(loginAction(loginData.email))
+    const navigate = useNavigate();
+    // const submitFn = (loginData: LoginData)=> {
+    //     console.log(JSON.stringify(loginData))
+    //     dispatch(loginAction(loginData.email))
+    // }
+    const loginWithFirebase = async (loginData: LoginData) => {
+        try{
+            const email = await login(loginData);
+            dispatch(loginAction(email));
+            navigate('/');
+        }catch(err){
+            console.log(err) //Todo
+        }
     }
-
 
     return (
         <div>
-            <SignIn submitFn={submitFn}/>
-            </div>
+            <SignIn submitFn={loginWithFirebase}/>
+        </div>
     );
 };
+
 
 export default Login;
